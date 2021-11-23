97fb96a1c53b3e7a18a81c2514b2199abbead97b
dywjd12jy <89719682+dywjd12jy@users.noreply.github.com>
2021-11-24 00:31:58 +0900

    탭버튼 querySelector, querySelectorAll 사용해보고 차이 알기

--- js_tab_button/main.js
+++ js_tab_button/main.js
@@ -1,5 +1,11 @@
-let tabBtn = document.querySelectorAll('.tab_btn');
-let tabMenu = document.querySelectorAll('.tab_menu');
-let tB_li = document.querySelectorAll('.tab_btn ul li');
-let tM_ul = document.querySelectorAll('.tab_menu ul');
+let tabBtn = document.querySelector('.tab_btn');
+let tabMenu = document.querySelector('.tab_menu');
+let tB_li = document.querySelector('.tab_btn ul li');
+let tM_ul = document.querySelector('.tab_menu ul');
+let tM_li = document.querySelector('.tab_menu ul li');
+
+tB_li.addEventListener("click", function(){
+    alert("hello");
+});
+console.log(tB_li);
 
