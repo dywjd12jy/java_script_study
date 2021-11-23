f83e7273c36fb796c90dd561d8c70ca281caf0e5
dywjd12jy <89719682+dywjd12jy@users.noreply.github.com>
2021-11-23 22:41:12 +0900

    js 탭버튼/탭메뉴 만들기 first

---
+++ js_tab_button/js_tab_button/main.js
@@ -0,0 +1,5 @@
+let tabBtn = document.querySelectorAll('.tab_btn');
+let tabMenu = document.querySelectorAll('.tab_menu');
+let tB_li = document.querySelectorAll('.tab_btn ul li');
+let tM_ul = document.querySelectorAll('.tab_menu ul');
+
